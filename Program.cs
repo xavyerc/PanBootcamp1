242807b86289411f08cf34bef0741c14fb5c3525
unknown <joseangel.1015@gmail.com>
2026-03-08 18:40:31 -0600

    Added boubble shield functionality

--- Program.cs
+++ Program.cs
@@ -13,13 +13,24 @@ int enemyMeeleAttack = 3;
 int enemyMagicAttack = 12;
 int enemyManaRecovery = 15;
 
+int boubbleShieldInv = 2;
+int boubbleShieldMax = 3;
+int boubbleShieldCD = 3;
+bool isBubbleShieldActive = false;
+
+int currentTurn = 0;
 
 void doDamage(int attack) {
  enemyHealth -= attack; // enemyHealth = enemyHealth - attack;
+ recoverMana();
 }
 
 void takeDamage (int attack) {
-  health -= attack; // health = health - attack;
+  if (!isBubbleShieldActive){
+    health -= attack; // health = health - attack;
+  } else {
+    Console.WriteLine("You blocked the attack succesfully!");
+  }
 }
 
 void recoverMana () {
@@ -37,15 +48,34 @@ void enemyRecoverMana () {
 
 }
 
+void boubbleShield ()
+{
+  if (boubbleShieldInv >= 1) {
+    isBubbleShieldActive = true;
+    boubbleShieldInv -= 1;
+  } else {
+    Console.WriteLine("Not enough boubble shields"); //TODO add CD timer in message here
+  }
+  recoverMana();
+}
+
+void regenerateBS () {
+  if ((currentTurn % boubbleShieldCD == 0) && (boubbleShieldInv < boubbleShieldMax) && (currentTurn != 0)) {
+    boubbleShieldInv += 1;
+  }
+}
+
 Random rnd = new Random();
 
 while (health > 0 && enemyHealth > 0) {
   Console.Clear();
+  Console.WriteLine("The current turn is: " + (currentTurn+1));
   Console.WriteLine("Health: " + health + "                 Enemy health: " + enemyHealth);
   Console.WriteLine("Mana: " + mana + "                 Enemy mana: " + enemyMana);
-  Console.WriteLine("Elige un ataque (meele = 1, magic = 2): ");
+  Console.WriteLine("Boubble shields available: " + boubbleShieldInv);
+  Console.WriteLine("Elige un ataque (meele = 1, magic = 2, Boubble shield mamalon = 3): ");
   string selection = Console.ReadLine();
-  if (selection == "2") {
+  if (selection == "2") { //TODO change if to switch
     if (mana >= manaCost) {
       doDamage(magicAttack);
       mana -= manaCost;
@@ -53,8 +83,9 @@ while (health > 0 && enemyHealth > 0) {
       Console.WriteLine("Not enough mana");
     }
   } else if (selection == "1") {
-    recoverMana();
     doDamage(meeleAttack);
+  } else if (selection == "3"){
+    boubbleShield();
   } else {
     Console.WriteLine("Invalid option, press enter to continue...");
     Console.ReadLine();
@@ -74,6 +105,11 @@ while (health > 0 && enemyHealth > 0) {
     enemyRecoverMana();
     takeDamage(enemyMeeleAttack);
   }
+
+  isBubbleShieldActive = false;
+  regenerateBS();
+
+  currentTurn += 1;
 }
 
 if (health <= 0) {
